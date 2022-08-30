3240f1dec220a70d5e75a854c1853dcb096d7b06
TripleS <triple-s-76@outlook.com>
2022-08-30 13:01:01 -0700

    Added ability to play multiple rounds at one time. Keeps score and displays each round info to console Displays final results to the console

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
+// Choose number of games to play here
+let numOfGames = 5;
+
+let playerScore = 0;
+let computerScore = 0;
+
 let computerChoice;
-getComputerChoice();
 
 // getComputerChoice() gets a random number (1, 2, 3) then converts it to Rock, Paper or Scissors
 function getComputerChoice() {
@@ -13,30 +18,28 @@ function getComputerChoice() {
     }
 }
 
-// playerChoiceAuto gets a random number (1, 2, 3)
-let playerChoiceAuto = Math.floor(Math.random() * 3) + 1;
-
 let playerChoice;
+let playerChoiceAutoNumber;
 
-// Changes playerChoiceAuto to Rock, Paper or Scissors & assigns to playerChoice
-playerSelectionConvert();
+// playerChoiceAuto() gets a random number (1, 2, 3) for the player
+function playerChoiceAuto() {
+    playerChoiceAutoNumber = Math.floor(Math.random() * 3) + 1;
+}    
 
 // playerSelectionConvert() converts 1, 2 or 3 to Rock, Paper or Scissors
 function playerSelectionConvert() {
-    if (playerChoiceAuto === 1) {
+    if (playerChoiceAutoNumber === 1) {
         playerChoice = 'Rock';
-    } else if (playerChoiceAuto === 2) {
+    } else if (playerChoiceAutoNumber === 2) {
         playerChoice = 'Paper';
     } else {
         playerChoice = 'Scissors';
     }
 }
 
-// Plays 1 round and gets the results in result
 let result;
-result = playRound(computerChoice, playerChoice);
 
-// playRound() plays 1 round and returns the results
+// playRound() plays 1 round, adds 1 to the winner's score and returns the results
 function playRound(computerChoice, playerChoice) {
     if (computerChoice == playerChoice) {
 
@@ -45,20 +48,26 @@ function playRound(computerChoice, playerChoice) {
     
         // Player chooses Rock
     } else if (playerChoice == 'Rock' && computerChoice == 'Paper') {
+        computerScore++;
         return 'Computer Wins! Computer\'s paper covered your rock.';
     } else if (playerChoice == 'Rock' && computerChoice == 'Scissors') {
+        playerScore++;
         return 'You Win! Your rock smashed computer\'s scissors.';
     
         // Player chooses Paper
     } else if (playerChoice == 'Paper' && computerChoice == 'Rock') {
+        playerScore++;
         return 'You Win! Your paper covered computer\'s rock.';
     } else if (playerChoice == 'Paper' && computerChoice == 'Scissors') {
+        computerScore++;
         return 'Computer Wins! Computer\'s scissors cut up your paper.';
     
         // Player chooses Scissors
     } else if (playerChoice == 'Scissors' && computerChoice == 'Rock') {
+        computerScore++;
         return 'Computer Wins! Computer\'s rock smashed your scissors.';
     } else if (playerChoice == 'Scissors' && computerChoice == 'Paper') {
+        playerScore++;
         return 'You Win! Your scissors cut up computer\'s paper.';
     
         // This should never run
@@ -67,10 +76,28 @@ function playRound(computerChoice, playerChoice) {
     }
 }
 
+game(numOfGames);
 
-// Player and computer choices logged to the console
-console.log('You chose ' + playerChoice);
-console.log('Computer chose ' + computerChoice);
+// Plays multiple games & keeps score, then displays result
+function game(num) {
+    for (let i = 0; i < num; i++) {
+        getComputerChoice();
+        playerChoiceAuto();
+        playerSelectionConvert();
+        result = playRound(computerChoice, playerChoice);
+        console.log(result);
+    }
 
-// Result logged to the console
-console.log(result);
+    // Display both scores to the console
+    console.log('Player Score: ' + playerScore);
+    console.log('Computer Score: ' + computerScore);
+
+    // Display final result in the console
+    if (playerScore === computerScore) {
+        console.log('It\'s a Tie! ' + playerScore + ' to ' + computerScore);
+    } else if (playerScore < computerScore) {
+        console.log('Computer Wins ' + computerScore + ' to ' + playerScore);
+    } else {
+        console.log('You Won ' + playerScore + ' to ' + computerScore);
+    }
+}
