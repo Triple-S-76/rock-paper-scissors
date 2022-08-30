19184a39b135fbe227e34371bef84361602ace62
TripleS <triple-s-76@outlook.com>
2022-08-30 10:03:50 -0700

    Added auto player selection choice

--- javascript.js
+++ javascript.js
@@ -5,11 +5,23 @@ function getComputerChoice() {
     let computerChoiceNum = Math.floor(Math.random() * 3) + 1;
     if (computerChoiceNum === 1) {
         computerChoice = 'Rock';
-    } else if (computerChoiceNum ===2) {
+    } else if (computerChoiceNum === 2) {
         computerChoice = 'Paper';
     } else {
         computerChoice = 'Scissors';
     }
 }
 
+let playerChoiceAuto = Math.floor(Math.random() * 3) + 1;
+let playerSelection;
+playerSelectionConvert();
 
+function playerSelectionConvert() {
+    if (playerChoiceAuto === 1) {
+        playerSelection = 'Rock';
+    } else if (playerChoiceAuto === 2) {
+        playerSelection = 'Paper';
+    } else {
+        playerSelection = 'Scissors';
+    }
+}
